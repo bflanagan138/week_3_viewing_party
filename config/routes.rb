70b276585a4b7c23b26e91f395c3188b9098b2fd
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-13 19:23:18 -0700

    feat/test: Adds log in happy path, completes US2

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/register', to: 'users#new'
-  get '/users', to: 'users#show'
-  post '/users', to: 'users#create'
   get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie'
 
--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
-    before :each do 
+    it 'has a header' do
         user1 = User.create(name: "User One", email: "user1@test.com", password: 'test1234')
         user2 = User.create(name: "User Two", email: "user2@test.com", password: 'pass1234')
         visit '/'
-        save_and_open_page
-    end 
-
-    it 'has a header' do
         expect(page).to have_content('Viewing Party Lite')
     end
 
     it 'has links/buttons that link to correct pages' do 
+        user1 = User.create(name: "User One", email: "user1@test.com", password: 'test1234')
+        user2 = User.create(name: "User Two", email: "user2@test.com", password: 'pass1234')
+        visit '/'
         click_button "Create New User"
         
         expect(current_path).to eq(register_path) 
@@ -27,7 +26,7 @@ RSpec.describe 'Landing Page' do
         user1 = User.create(name: "User One", email: "user1@test.com", password: 'test1234')
         user2 = User.create(name: "User Two", email: "user2@test.com", password: 'pass1234')
        
-
+        visit '/'
         expect(page).to have_content('Existing Users:')
 
         within('.existing-users') do 
@@ -37,9 +36,31 @@ RSpec.describe 'Landing Page' do
     end 
 
     it 'has a log in link' do 
+        user1 = User.create!(name: "User One", email: "user1@test.com", password: 'test1234', password_confirmation: 'test1234')
+
         visit '/'
 
         click_link "Log In"
         expect(current_path).to eq(login_path)
+        fill_in :email, with:'user1@test.com'
+        fill_in :password, with:'test1234'
+    
+        click_button "Log In"
+        expect(current_path).to eq(user_path(user1))
+    end  
+
+    it 'has a log in link, when I provide incorrect credentials I get errors' do 
+        user1 = User.create!(name: "User One", email: "user1@test.com", password: 'test1234', password_confirmation: 'test1234')
+
+        visit '/'
+
+        click_link "Log In"
+        expect(current_path).to eq(login_path)
+        fill_in :email, with:'user2@test.com'
+        fill_in :password, with:'test1234'
+    
+        click_button "Log In"
+        expect(current_path).to eq(login_path)
+        save_and_open_page
     end  
 end
